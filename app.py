35637ce0a1c6c17312a5a18b9d533505dd65d8ac
Алексей Пискунов <piskunov.alesha@gmail.com>
2020-08-11 23:51:33 +0700

    fixed bugs

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import jinja2
 import json
 import os
 import random
+import time
 import datetime
 import aiopg.sa
 import typing
+import base64
 from bs4 import BeautifulSoup
+from aiomisc import timeout, threaded
 
 db: typing.Optional[aiopg.sa.engine.Engine] = None
 RULE = {
@@ -148,29 +151,61 @@ def check(field):
     return True
 
 
-def solve(field, i=0, j=0):
-    if i == 9 and j == 0:
-        return True
-    next_i, next_j = (i + 1, 0) if (j == 8) else (i, j + 1)
-    if field[i][j] is not None:
-        return solve(field, next_i, next_j)
-    for k in range(1, 10):
-        field[i][j] = k
-        if check(field) and solve(field, next_i, next_j):
-            return True
-    field[i][j] = None
-    return False
+@timeout(45)  # sec
+async def solve(field_):
+    @threaded
+    def _solve(field):
+        def wrapper(i, j):
+            if i == 9 and j == 0:
+                return True
+            next_i, next_j = (i + 1, 0) if (j == 8) else (i, j + 1)
+            if field[i][j] is not None:
+                return wrapper(next_i, next_j)
+            for k in range(1, 10):
+                field[i][j] = str(k)
+                if check(field) and wrapper(next_i, next_j):
+                    return True
+            field[i][j] = None
+            return False
+        return wrapper(0, 0)
+    return await _solve(field_)
+
+
+def pretty_print(field):
+    def it_():
+        for row in field:
+            for e in row:
+                yield e or '.'
+    it = it_()
+    def get_three():
+        return ' '.join([next(it) for _ in range(3)])
+    for i in range(3):
+        for _ in range(3):
+            print(get_three(), '|', get_three(), '|', get_three())
+        if i != 2:
+            print('-' * (9 + 2 + 2 * 5))
 
 
 async def sudoku_handler(request):
+    start_at = time.time()
     ws = aiohttp.web.WebSocketResponse()
     await ws.prepare(request)
     async for msg in ws:
         field = parse_field(msg.data)
-        if solve(field):
-            await ws.send_json(field)
-        else:
+        print('New query:'); pretty_print(field)
+        print(base64.encodebytes(json.dumps(field).encode()).decode())
+        try:
+            if await solve(field):
+                await ws.send_json(field)
+                print('Result:'); pretty_print(field)
+            else:
+                await ws.send_json(dict(result=False))
+                print('Result not found')
+        except TimeoutError:
             await ws.send_json(dict(result=False))
+            print('Timeout request')
+        print(f'at {time.time() - start_at} sec')
+        await ws.close()
 
 
 if __name__ == '__main__':
